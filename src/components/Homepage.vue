4ae9af5d6d030ede5926132fc6dfa1c139f70d0e
Unknown <wengpeter2006@gmail.com>
2018-12-04 23:00:04 +0800

    update

---
+++ src/components/Homepage.vue
@@ -0,0 +1,24 @@
+<template>
+    <Layout>
+        <Header>
+        </Header>
+        <Content>
+            <Card>
+                <p></p>
+                <p></p>
+                
+            </Card>
+        </Content>
+        <Footer>
+        </Footer>
+    </Layout>
+</template>
+<script>
+export default {
+     
+}
+</script>
+
+<style>
+
+</style>
